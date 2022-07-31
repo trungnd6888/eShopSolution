1a2a7f62bbf3ed3dcbdd584f6fe2d1cae28ef1b3
Trung Nguyen <trungk47s5@gmail.com>
2022-07-31 23:24:11 +0700

    update

--- eShopSolution.BackendApi/Controllers/ProductsController.cs
+++ eShopSolution.BackendApi/Controllers/ProductsController.cs
@@ -9,7 +9,6 @@ namespace eShopSolution.BackendApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
     public class ProductsController : ControllerBase
     {
         private readonly IManageProductService _manageProductService;
